444104a9407514c095508619815d3273bbb1a590
kotaroyamame <yamame@fi.chu.jp>
2019-12-10 21:50:10 +0900

    ###

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,6 @@
-import Vue from 'vue'
-declare module 'vue/types/vue' {
-	interface Vue {
-		$ticket: any;
-	}
-}
+// import Vue from 'vue'
+// declare module 'vue/types/vue' {
+// 	interface Vue {
+// 		$ticket: any;
+// 	}
+// }
