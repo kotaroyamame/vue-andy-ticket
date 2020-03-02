84c5ce55977be3d6a78a3af867c2c01a0c27fbe2
Kotaro <info@iid.systems>
2020-03-02 21:40:09 +0900

    fix types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,19 @@
-// import Vue from 'vue'
-// declare module 'vue/types/vue' {
-// 	interface Vue {
-// 		$ticket: any;
-// 	}
-// }
+
+import Vue from "vue";
+
+declare module "vue/types/vue" {
+	interface TicketService {
+		setItem(item: any): void;
+		setQuery(text: string): void;
+		setStartTime(time?: string): void;
+		setEndTime(time?: string): void;
+		setData(data: any): void;
+		push(): Promise<any>;
+		reset(): void;
+	}
+	interface Vue {
+		$ticket: TicketService;
+	}
+}
+
+
